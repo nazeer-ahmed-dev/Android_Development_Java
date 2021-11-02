77ffe83dfec50f7153064367e65afa02039b90df
unknown <nazeerahmedofficial1.com>
2021-11-02 12:36:27 +0500

    reading image from camera done

---
+++ ReadingPicFromCamera/app/src/main/java/com/example/readingpicfromcamera/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.readingpicfromcamera;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    final int code = 3;
+    ImageView img ;
+   // @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageView);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            //Do Something
+            startActivityForResult(intent,code);
+        }
+        else{
+            //request Permission Here
+           if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+           {
+               requestPermissions(new String[]{Manifest.permission.CAMERA},code);
+           }
+            //startActivityForResult(intent,code);
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==code && resultCode==RESULT_OK)
+        {
+            if(requestCode==code && resultCode==RESULT_OK)
+            {
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                img.setImageBitmap(bmp);
+            }
+        }
+    }
+}
