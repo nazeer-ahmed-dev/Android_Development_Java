0a3af3d298ce4409494ae86eb3258cee61e0a022
unknown <nazeerahmedofficial1.com>
2021-11-23 20:57:24 +0500

    floatingMenu

---
+++ CreatingFloatingMenu/app/src/main/java/com/example/floatingmenu/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.floatingmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        registerForContextMenu(btn);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("ContextMenu");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.float_menu,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        return super.onContextItemSelected(item);
+    }
+}
