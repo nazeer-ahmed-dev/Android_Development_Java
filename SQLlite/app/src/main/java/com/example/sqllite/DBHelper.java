1311e757229c829a60db56966f0f6c0b06227608
unknown <nazeerahmedofficial1.com>
2022-01-10 17:34:07 +0500

    SQlite

--- SQLlite/app/src/main/java/com/example/sqllite/DBHelper.java
+++ SQLlite/app/src/main/java/com/example/sqllite/DBHelper.java
@@ -7,38 +7,28 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
-
     public final static String DBNAME = "TestDB";
     public final static int VERSION = 1;
-
     public DBHelper(Context context) {
         super(context,DBNAME,null,VERSION);
     }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-
         String query = "CREATE TABLE LOGIN (ID INTEGER , USERNAME TEXT , PASSWORD TEXT)";
         db.execSQL(query);
-
         ContentValues cv = new ContentValues();
         cv.put("ID",1);
         cv.put("USERNAME","NAZEER");
         cv.put("PASSWORD","123");
 
         db.insert("LOGIN",null,cv);
-
     }
-
     @Override
     public void onConfigure(SQLiteDatabase db) {
         super.onConfigure(db);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
     }
     public void insertInto(int id , String name , String paswword)
     {
@@ -48,7 +38,6 @@ public class DBHelper extends SQLiteOpenHelper {
         cv.put("USERNAME",name);
         cv.put("PASSWORD",paswword);
         db.insert("LOGIN",null,cv);
-
     }
     public Cursor getUSErBYid()
     {
